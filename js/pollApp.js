2330606fbebfc07587cd955dc5e366dcbf268e85
plato-cambrian <plato@cambrian.org>
2014-07-15 18:23:51 -0400

    MOAR TEST

--- js/pollApp.js
+++ js/pollApp.js
@@ -1,7 +1,21 @@
 console.log(angular);
-var helloModule = angular.module("pollApp", []) // array is required
+var helloModule = angular.module("pollApp", ["ngRoute"]) // array is required
+
+helloModule.config(function($routeProvider){
+  $routeProvider.when("/peerLists", {
+    templateUrl: "/partials/peerLists.html",
+    controller: "peerListsCtrl"
+  });
+});
   
-helloModule.controller("pollAppCtrl", function($scope){
+helloModule.controller("pollAppCtrl", function($scope, $location){
+    $scope.peerListsShow = function(){
+      $location.path("/peerLists");
+    };
+    console.log($scope);
+})
+
+helloModule.controller("peerListsCtrl", function($scope){
     console.log($scope);
 })
 
--- test/e2e/pollAppCtrlSpec.js
+++ test/e2e/pollAppCtrlSpec.js
@@ -13,30 +13,37 @@ describe("pollAppCtrl", function () {
 	});
 
 	it("shows the peer lists management screen when the peer lists button is clicked", function () {
-
+    element(by.id('peerListsButton')).click();
+		expect($("p.test").isDisplayed()).toBeTruthy;
 	});
 
 	it("has a manage templates button", function () {
-
+    var manageTemplates = element(by.id("manageTemplatesButton"));
+		expect(manageTemplates.getText()).toEqual("Manage Templates");
 	});
 
 	it("shows the manage templates screen when the manage templates button is clicked", function () {
-
+    element(by.id('manageTemplatesButton')).click();
+		expect($("p.test").isDisplayed()).toBeTruthy;
 	});
 
 	it("has a create new poll button", function () {
-
+    var createPoll = element(by.id("createPollButton"));
+		expect(createPoll.getText()).toEqual("Create Poll");
 	});
 
 	it("shows the create new poll screen when the create new poll button is clicked", function () {
-
+    element(by.id('createPollButton')).click();
+		expect($("p.test").isDisplayed()).toBeTruthy;
 	});
 
 	it("has a help button", function () {
-
+    var help = element(by.id("helpButton"));
+		expect(help.getText()).toEqual("Help");
 	});
 
 	it("shows the help screen when the help button is clicked", function () {
-
+    element(by.id('helpButton')).click();
+		expect($("p.test").isDisplayed()).toBeTruthy;
 	});
 });
