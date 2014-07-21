d441d517795d58a1fecbbc0c6a1f1960dd2fdcd7
voodoo <voodoo@blackandyellowpages.com>
2014-07-21 17:15:04 -0500

    yay tests work

--- test/e2e/pollsListingHeaderSpec.js
+++ test/e2e/pollsListingHeaderSpec.js
@@ -1,48 +1,58 @@
-browser.get("default.htm");
+var PollsListHeader = function () {
+	this.pollHeader = element(by.id('pollHeader'));
+	this.logo = element(by.css("img"));
+	this.peerLists = element(by.id("peerListsButton"));
+	this.manageTemplates = element(by.id("manageTemplatesButton"));
+	this.createPoll = element(by.id("createPollButton"));
+    this.help = element(by.id("helpButton"));
+};
 
 describe("polls listing header", function () {
+	var pollsListHeader;
+
+	it("sets up the page for testing", function () {
+		browser.get("default.htm");
+		pollsListHeader = new PollsListHeader();
+		expect(pollsListHeader.hasOwnProperty("pollHeader")).toEqual(true);
+	});
 		
 	it("has a logo on the far left", function () {
-		var logo = element(by.css("img"));
-		expect(logo.isDisplayed()).toBeTruthy();
+		expect(pollsListHeader.logo.isDisplayed()).toBeTruthy();
 	});
 
 	it("has a peer lists button", function () {
-    var peerLists = element(by.id("peerListsButton"));
-		expect(peerLists.getText()).toEqual("Peer Lists");
+		expect(pollsListHeader.peerLists.getText()).toEqual("Peer Lists");
 	});
 
 	it("shows the peer lists management screen when the peer lists button is clicked", function () {
-    element(by.id('peerListsButton')).click();
+    	element(by.id('peerListsButton')).click();
 		expect($("p.test").isDisplayed()).toBeTruthy();
 	});
 
 	it("has a manage templates button", function () {
-    var manageTemplates = element(by.id("manageTemplatesButton"));
-		expect(manageTemplates.getText()).toEqual("Manage Templates");
+		expect(pollsListHeader.manageTemplates.getText()).toEqual("Manage Templates");
 	});
 
 	it("shows the manage templates screen when the manage templates button is clicked", function () {
-    element(by.id('manageTemplatesButton')).click();
+    	element(by.id('manageTemplatesButton')).click();
 		expect($("p.test").isDisplayed()).toBeTruthy();
 	});
 
 	it("has a create new poll button", function () {
-    var createPoll = element(by.id("createPollButton"));
-		expect(createPoll.getText()).toEqual("Create Poll");
+		expect(pollsListHeader.createPoll.getText()).toEqual("Create Poll");
 	});
 
 	it("shows the create new poll screen when the create new poll button is clicked", function () {
-    element(by.id('createPollButton')).click();
+    	element(by.id('createPollButton')).click();
 		expect($("p.test").isDisplayed()).toBeTruthy();
 	});
 
 	it("has a help button", function () {
-    var help = element(by.id("helpButton"));
+    	expect(pollsListHeader.help.isDisplayed()).toBeTruthy();
 	});
 
 	it("shows the help screen when the help button is clicked", function () {
-    element(by.id('helpButton')).click();
+    	element(by.id('helpButton')).click();
 		expect($("p.test").isDisplayed()).toBeTruthy();
 	});
 });
--- test/e2e/pollsListingSpec.js
+++ test/e2e/pollsListingSpec.js
@@ -1,119 +1,132 @@
-browser.get('default.htm');
+var PollsListPanel = function () {
+
+	this.entries = element.all(by.repeater("poll in polls"));
+	this.listContainer = element(by.id('pollsListContainer'));
+	
+    this.completedStatus = this.entries.get(0).findElement(by.binding("poll.status"));
+	this.completedTotalPolled = this.entries.get(0).findElement(by.binding("poll.nRecipients"));
+	this.completedTotalResponded = this.entries.get(0).findElement(by.binding("poll.nResponses"));
+	this.completedPollDate = this.entries.get(0).findElement(by.binding("poll.dateStarted"));
+	this.completedPollType = this.entries.get(0).findElement(by.binding("poll.type"));
+	this.completedPollTitle = this.entries.get(0).findElement(by.binding("poll.title"));
+	this.completedCopyButton = this.entries.get(0).findElement(by.css(".copyPollButton"));
+	this.completedDeleteButton = this.entries.get(0).findElement(by.css(".deletePollButton"));
+	this.startedViewResultsButton = this.entries.get(0).findElement(by.css(".viewResultsButton"));
+	this.completedViewResultsButton = this.entries.get(2).findElement(by.css(".viewResultsButton"));
+	this.startedEditPollButton = this.entries.get(0).findElement(by.css(".editPollButton"));
+	this.completedEditPollButton = this.entries.get(2).findElement(by.css(".editPollButton"));
+	this.startedStartPollButton = this.entries.get(0).findElement(by.css(".startPollButton"));
+	this.completedStartPollButton = this.entries.get(2).findElement(by.css(".startPollButton"));
+	this.unstartedEditPollButton = this.entries.get(3).findElement(by.css(".editPollButton"));
+	this.unstartedStartPollButton = this.entries.get(3).findElement(by.css(".startPollButton"));
+	this.unstartedTotalPolled = this.entries.get(3).findElement(by.binding("poll.nRecipients"));
+	this.unstartedTotalResponded = this.entries.get(3).findElement(by.binding("poll.nResponses"));
+	this.unstartedPollDate = this.entries.get(3).findElement(by.binding("poll.dateStarted"));
+	this.unstartedViewResultsButton = this.entries.get(3).findElement(by.css(".viewResultsButton"));
+};
 
 describe("polls listing", function () {
 
-	it("has a listing of all saved polls", function () {
-		var listContainer = element(by.id('pollsListContainer'));
-		expect(listContainer.isDisplayed()).toBeTruthy();
+	var pollsListPanel; 
+	it("sets up the second page for tests", function(){
+		browser.get('default.htm')
+		pollsListPanel = new PollsListPanel();
+        expect(pollsListPanel.hasOwnProperty("listContainer")).toEqual(true);		
+	});
+
+	it("displays a listing of all saved polls", function () {
+		expect(pollsListPanel.listContainer.isDisplayed()).toBeTruthy();
 	});
 
 	describe("entry", function () {
 
-		var entries = element.all(by.repeater("poll in polls"));
-		var status = entries.get(0).findElement(by.binding("poll.status"));
-		var totalPolled = entries.get(0).findElement(by.binding("poll.nRecipients"));
-		var totalResponded = entries.get(0).findElement(by.binding("poll.nResponses"));
-		var pollDate = entries.get(0).findElement(by.binding("poll.dateStarted"));
-		var pollType = entries.get(0).findElement(by.binding("poll.type"));
-		var pollTitle = entries.get(0).findElement(by.binding("poll.title"));
-
 		it("displays the status", function () {
-			expect(status.isDisplayed).toBeTruthy();
-			expect(status.getText()).toEqual("started");
+			browser.debugger();
+			expect(pollsListPanel.completedStatus.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedStatus.getText()).toEqual("started");
 		});
 		
 		it("displays the total number polled", function () {
-			expect(totalPolled.isDisplayed).toBeTruthy();
-			expect(totalPolled.getText()).toEqual("100");
+			expect(pollsListPanel.completedTotalPolled.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedTotalPolled.getText()).toEqual("100");
 		});
 
 		it("displays the total number responded", function () {
-			expect(totalResponded.isDisplayed).toBeTruthy();
-			expect(totalResponded.getText()).toEqual("82");
+			expect(pollsListPanel.completedTotalResponded.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedTotalResponded.getText()).toEqual("82");
 		});
 		
 		it("displays the poll date", function () {
-			expect(pollDate.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedPollDate.isDisplayed).toBeTruthy();
 		});
 		
 		it("displays the poll type", function () {
-			expect(pollType.isDisplayed).toBeTruthy();
-			expect(pollType.getText()).toEqual("Battle Ping:");
+			expect(pollsListPanel.completedPollType.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedPollType.getText()).toEqual("Battle Ping:");
 		});
 		
 		it("displays the poll title", function () {
-			expect(pollTitle.isDisplayed).toBeTruthy();
-			expect(pollTitle.getText()).toEqual("Join Operation Red Dawn!");
+			expect(pollsListPanel.completedPollTitle.isDisplayed).toBeTruthy();
+			expect(pollsListPanel.completedPollTitle.getText()).toEqual("Join Operation Red Dawn!");
 		});
 
 		it("has a copy button", function () {
-			var copyButton = entries.get(0).findElement(by.css(".copyPollButton"));
-			expect(copyButton.isDisplayed()).toBeTruthy();
-			expect(copyButton.getText()).toEqual("Copy");
+			expect(pollsListPanel.completedCopyButton.isDisplayed()).toBeTruthy();
+			expect(pollsListPanel.completedCopyButton.getText()).toEqual("Copy");
 		});
 
 		it("has a delete button", function () {
-			var deleteButton = entries.get(0).findElement(by.css(".deletePollButton"));
-			expect(deleteButton.isDisplayed()).toBeTruthy();
-			expect(deleteButton.getText()).toEqual("Delete");
+			expect(pollsListPanel.completedDeleteButton.isDisplayed()).toBeTruthy();
+			expect(pollsListPanel.completedDeleteButton.getText()).toEqual("Delete");
 		});
 
 		describe("that is unstarted", function() {
 
 			it("has an edit poll button", function () {
-				var editPollButton = entries.get(3).findElement(by.css(".editPollButton"));
-				expect(editPollButton.isDisplayed()).toBeTruthy();
-				expect(editPollButton.getText()).toEqual("Edit Poll");
+				expect(pollsListPanel.unstartedEditPollButton.isDisplayed()).toBeTruthy();
+				expect(pollsListPanel.unstartedEditPollButton.getText()).toEqual("Edit Poll");
 			});
 
 			it("has a start poll button", function () {
-				var startPollButton = entries.get(3).findElement(by.css(".startPollButton"));
-				expect(startPollButton.isDisplayed()).toBeTruthy();
-				expect(startPollButton.getText()).toEqual("Start Poll");
+				expect(pollsListPanel.unstartedStartPollButton.isDisplayed()).toBeTruthy();
+				expect(pollsListPanel.unstartedStartPollButton.getText()).toEqual("Start Poll");
 			});
 
-			it("does not display response data", function () {
-				var totalPolled = entries.get(3).findElement(by.binding("poll.nRecipients"));
-				var totalResponded = entries.get(3).findElement(by.binding("poll.nResponses"));
-				expect(totalPolled.isDisplayed()).toBeFalsy();
-				expect(totalResponded.isDisplayed()).toBeFalsy();
+			it("does not display number of responses", function () {
+				expect(pollsListPanel.unstartedTotalPolled.isDisplayed()).toBeFalsy();
+			});
+
+			it("does not display number polled", function () {
+				expect(pollsListPanel.unstartedTotalResponded.isDisplayed()).toBeFalsy();
 			});
 
 			it("does not display the poll date", function () {
-				var pollDate = entries.get(3).findElement(by.binding("poll.dateStarted"))
-				expect(pollDate.isDisplayed()).toBeFalsy();
+				expect(pollsListPanel.unstartedPollDate.isDisplayed()).toBeFalsy();
 			});
 
 			it("does not display the view results button", function () {
-				var viewResultsButton = entries.get(3).findElement(by.css(".viewResultsButton"));
-				expect(viewResultsButton.isDisplayed()).toBeFalsy();
-			})
+				expect(pollsListPanel.unstartedViewResultsButton.isDisplayed()).toBeFalsy();
+			});
 
 		});
 
 		describe("that is started or completed", function () {
 
 			it("has a view results button", function() {
-				var startedViewResultsButton = entries.get(0).findElement(by.css(".viewResultsButton"));
-				var completedViewResultsButton = entries.get(2).findElement(by.css(".viewResultsButton"));
-				expect(startedViewResultsButton.isDisplayed()).toBeTruthy();
-				expect(startedViewResultsButton.getText()).toEqual("View Results");
-				expect(completedViewResultsButton.isDisplayed()).toBeTruthy();
-				expect(completedViewResultsButton.getText()).toEqual("View Results");
+				expect(pollsListPanel.startedViewResultsButton.isDisplayed()).toBeTruthy();
+				expect(pollsListPanel.startedViewResultsButton.getText()).toEqual("View Results");
+				expect(pollsListPanel.completedViewResultsButton.isDisplayed()).toBeTruthy();
+				expect(pollsListPanel.completedViewResultsButton.getText()).toEqual("View Results");
 			});
 
 			it("does not show edit poll button", function () {
-				var startedEditPollButton = entries.get(0).findElement(by.css(".editPollButton"));
-				var completedEditPollButton = entries.get(2).findElement(by.css(".editPollButton"));
-				expect(startedEditPollButton.isDisplayed()).toBeFalsy();
-				expect(completedEditPollButton.isDisplayed()).toBeFalsy();
+				expect(pollsListPanel.startedEditPollButton.isDisplayed()).toBeFalsy();
+				expect(pollsListPanel.completedEditPollButton.isDisplayed()).toBeFalsy();
 			});
 
 			it("does not show start poll button", function () {
-				var startedStartPollButton = entries.get(0).findElement(by.css(".startPollButton"));
-				var completedStartPollButton = entries.get(2).findElement(by.css(".startPollButton"));
-				expect(startedStartPollButton.isDisplayed()).toBeFalsy();
-				expect(completedStartPollButton.isDisplayed()).toBeFalsy();
+				expect(pollsListPanel.startedStartPollButton.isDisplayed()).toBeFalsy();
+				expect(pollsListPanel.completedStartPollButton.isDisplayed()).toBeFalsy();
 			});
 		});
 
